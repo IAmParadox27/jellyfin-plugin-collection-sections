3f87e02633b12cef8d37f23975bdd6fde35f9eaa
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-02-09 18:23:40 +0000

    Forgot to update Version/AssemblyVersion values

--- src/Jellyfin.Plugin.CollectionSections/Properties/AssemblyInfo.cs
+++ src/Jellyfin.Plugin.CollectionSections/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@
 [assembly: AssemblyProduct("Jellyfin.Plugin.CollectionSections")]
 [assembly: AssemblyDescription("Adds ability to add home screen sections for collections and playlists to Jellyfin.")]
 [assembly: AssemblyTitle("Jellyfin.Plugin.CollectionSections")]
-[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
