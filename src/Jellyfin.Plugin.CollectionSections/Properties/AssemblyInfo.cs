8e3ab9799067396f12ed6494cf7ef1996e211603
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-08-23 01:35:57 +0100

    Adding a framework for being able to support multiple jellyfin versions simultaneously without having to stop support for previous versions when a new one releases.

--- src/Jellyfin.Plugin.CollectionSections/Properties/AssemblyInfo.cs
+++ src/Jellyfin.Plugin.CollectionSections/Properties/AssemblyInfo.cs
@@ -1,6 +1,9 @@
 ﻿using System.Reflection;
+using Jellyfin.Plugin.CollectionSections.Attributes;
+
 [assembly: AssemblyCompany("IAmParadox27")]
 [assembly: AssemblyProduct("Jellyfin.Plugin.CollectionSections")]
 [assembly: AssemblyDescription("Adds ability to add home screen sections for collections and playlists to Jellyfin.")]
 [assembly: AssemblyTitle("Jellyfin.Plugin.CollectionSections")]
 [assembly: AssemblyVersion("2.0.0.0")]
+[assembly: JellyfinVersion("10.10.7")]
